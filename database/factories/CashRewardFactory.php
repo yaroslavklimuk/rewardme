2d76c22e581349219a2f59420c1d6b1ab66d9023
Yaroslav Klimuk <ya.klimuk@penguins.ru>
2020-03-11 10:01:46 +0300

    fix db factories

--- database/factories/CashRewardFactory.php
+++ database/factories/CashRewardFactory.php
@@ -2,16 +2,12 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
-use App\Models\CashReward;
 use Faker\Generator as Faker;
 
-$factory->define(CashReward::class, function (Faker $faker) {
+$factory->define(\App\Models\CashReward::class, function (Faker $faker) {
     return [
-        return [
             'user_id' => $faker->randomDigit,
             'amount' => $faker->randomDigit,
             'payed' => false
-        ];
     ];
 });
